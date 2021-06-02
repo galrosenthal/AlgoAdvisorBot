9ac7005f3bcf66e4516b822bb0e96a7c97efe44e
Gal <galrosenthal@gmail.com>
2021-06-02 18:33:59 +0300

    added help message, added commands to the bot
    added README.md

--- Include/main.py
+++ Include/main.py
@@ -1,15 +1,76 @@
+from typing import List
+
 from telegram.ext import *
 from telegram.ext.dispatcher import run_async
+from telegram import BotCommand
 import advisor as adv
 import CONSTANTS
 import logging as logger
 
+# list of Available commands
+AVAILABLE_COMMANDS = [
+    {
+        'name': 'start',
+        'description': 'Start a new chat with the bot'
+    },
+    {
+        'name': 'help',
+        'description': 'Print the help message'
+    },
+    {
+        'name': 'general',
+        'description': 'General portfolio with multi field stocks'
+    },
+    {
+        'name': 'health',
+        'description': 'Health technology portfolio'
+    },
+    {
+        'name': 'utils',
+        'description': 'Utilities portfolio'
+    },
+    {
+        'name': 'energy_minerals ',
+        'description': 'Energy and Minerals portfolio'
+    },
+    {
+        'name': 'finance',
+        'description': 'Finance portfolio'
+    },
+    {
+        'name': 'tech_services',
+        'description': 'Technology services portfolio'
+    }
+]
+
+
+def help_text():
+    help_str = "\n\n\nWelcome to BotAdvisor,\n" \
+               "This bot is advising you for the best trading portfolio " \
+               "based on a specific field of interest, Technology, Health, etc." \
+               "The supported commands:\n" \
+               "1. /start - Start a new chat with the bot \n" \
+               "2. /help - Print the help message \n" \
+               "3. /general - General portfolio with multi field stocks\n" \
+               "4. /health - Health technology portfolio\n" \
+               "5. /utils - Utilities portfolio\n" \
+               "6. /energy_minerals - Energy and Minerals portfolio\n" \
+               "7. /finance - Finance portfolio\n" \
+               "8. /tech_services - Technology services portfolio"
+    return help_str
 
 def start(update, context):
-    # TODO: Add explaination of the bot
     logger.info('New Session Started')
     str_val = "Welcome " + str(update.message.chat_id) + ', Lets start'
+    str_val += help_text()
     update.message.reply_text(str_val)
+
+
+def help(update, context):
+    logger.info("Help print")
+    text = help_text()
+    update.message.reply_text(text)
+
 
 def general(update, context):
     logger.info('New general Question')
@@ -53,6 +114,15 @@ def tech_services(update, context):
     context.bot.sendPhoto(chat_id=update.message.chat.id, photo=open(str(update.message.chat_id) + '.png', 'rb'))
 
 
+def get_our_bot_commands():
+    bot_commands: List[BotCommand] = []
+    for command in AVAILABLE_COMMANDS:
+        new_bot_command = BotCommand(command['name'], command['description'])
+        bot_commands.append(new_bot_command)
+
+    return bot_commands
+
+
 def main():
     logger.basicConfig(level=logger.INFO, format='%(asctime)s | %(levelname)s | %(message)s', datefmt='%d/%m/%Y-%H:%M:%S')
 
@@ -60,7 +130,10 @@ def main():
     updater = Updater(CONSTANTS.API_KEY, use_context=True, workers=6)
 
     dp = updater.dispatcher
+    avail_bot_commands = get_our_bot_commands()
+    dp.bot.set_my_commands(avail_bot_commands)
     dp.add_handler(CommandHandler('start', start, run_async=True))
+    dp.add_handler(CommandHandler('help', help, run_async=True))
     dp.add_handler(CommandHandler('general', general, run_async=True))
     dp.add_handler(CommandHandler('health', health_tech, run_async=True))
     dp.add_handler(CommandHandler('utils', utils, run_async=True))
---
+++ README.md
@@ -0,0 +1,34 @@
+<h1 align="center">Welcome to AlgoAdviosrBot 👋</h1>
+
+
+> Telegram bot that will generate trading portfolio by specific field
+> health, general, finance and more.
+
+## 🚀 Usage
+
+To Use this bot, enter telegram and search for
+`` @AlgoAdvisorBot``
+
+## Authors
+
+👤 **Gal Rosenthal**
+    - Github: [@galrosenthal](https://github.com/galrosenthal)
+
+👤 **Noy Harari**
+    - Github: [@noyharar](https://github.com/noyharar)
+
+👤 **Noa Gorengot**
+    - Github: [@noagorengot](https://github.com/gorenNoa)
+
+👤**Hila Cohen**
+    - Github: [@hilacohen](https://github.com/hila5)
+
+## 📝 License
+
+Copyright © 2021
+
+This project is [MIT](https://github.com/kefranabg/readme-md-generator/blob/master/LICENSE) licensed.
+
+---
+
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
